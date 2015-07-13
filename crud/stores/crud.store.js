25e958e5cc290190cf5229ad712831f68d339ca2
ramonhenrique <ramon_henrique34@hotmail.com>
2015-07-13 13:46:28 -0300

    removendo setTimeout

--- crud/stores/crud.store.js
+++ crud/stores/crud.store.js
@@ -90,46 +90,42 @@ function DB() {
         ];
 
     this.find = function (id, callback) {
-        setTimeout(function () {
-            var dado = db.reduce(function (ret, item) {
-                if (!ret && item._id == id)
-                    return item;
-                return ret;
-            }, null);
-            if (dado.mae)
-                dado={
-                    _id: dado._id,
-                    name: dado.name,
-                    doc: dado.doc,
-                    sexo: dado.sexo,
-                   mae : {
-                      _id: dado.mae,
-                      display: db.reduce(function (ret, item) {
-                          if (!ret && item._id == dado.mae)
-                              return item.name+' '+item.doc;
-                          return ret;
-                      }, null)
-                   }
-                };
-            callback(null, dado);
-        }, 0);
+        var dado = db.reduce(function (ret, item) {
+            if (!ret && item._id == id)
+                return item;
+            return ret;
+        }, null);
+        if (dado.mae)
+            dado={
+                _id: dado._id,
+                name: dado.name,
+                doc: dado.doc,
+                sexo: dado.sexo,
+               mae : {
+                  _id: dado.mae,
+                  display: db.reduce(function (ret, item) {
+                      if (!ret && item._id == dado.mae)
+                          return item.name+' '+item.doc;
+                      return ret;
+                  }, null)
+               }
+            };
+        callback(null, dado);
     };
 
     this.search = function (searchText, callback) {
-        setTimeout(function () {
-            callback(null, db.reduce(function (ret, item) {
-                var itemName = item.name.toUpperCase();
-                var searchTextaux = searchText.toUpperCase();
-                if (searchText === '' || itemName.indexOf(searchTextaux) >= 0 || item._id == searchText) {
-                    ret.push({
-                        _id: item._id,
-                        name: item.name,
-                        sexo: item.sexo
-                    });
-                }
-                return ret;
-            }, []));
-        }, 0);
+        callback(null, db.reduce(function (ret, item) {
+            var itemName = item.name.toUpperCase();
+            var searchTextaux = searchText.toUpperCase();
+            if (searchText === '' || itemName.indexOf(searchTextaux) >= 0 || item._id == searchText) {
+                ret.push({
+                    _id: item._id,
+                    name: item.name,
+                    sexo: item.sexo
+                });
+            }
+            return ret;
+        }, []));
     }
 
     this.save = function (listing, editing, callback) {
